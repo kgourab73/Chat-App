6191e45e67430f0636c225ce7fdbdc613da400fc
Kirtigourab Gouda <98795248+kgourab73@users.noreply.github.com>
2024-12-09 23:44:42 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -43,17 +43,5 @@ export default {
       "sunset",
     ],
   },
-};/** @type {import('tailwindcss').Config} */
-
-import daisyui from 'daisyui'
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-}
+};
 
